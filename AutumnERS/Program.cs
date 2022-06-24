bec2f4c2f5f4092f839cbb158b06871f4ec65fcb
Jay Sprout <jotasprout@gmail.com>
2022-06-23 20:58:23 -0400

    Create outline based on diagrams, use cases, user stories

--- AutumnERS/Program.cs
+++ AutumnERS/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Database address and creds kept elsewhere
+    // Table for Tickets
+    // Table for Users
+
+// SQL file containing all necessary queries
+
+// TICKETS
+    // Create Ticket Class
+    // Ticket states
+        // Pending
+        // Approved
+        // Denied
+    // ticket creation
+    // ticket review
+        // View by Manager
+            // View tickets from everyone
+                // Filter requests by status
+            // View tickets from individual
+                // approve
+                // Deny
+
+        // View by Employee
+            // approval request
+
+// USERS
+    // Create User class
+        // Manager
+        // Employee
+    // User creation
+        // User login
+
+// 
