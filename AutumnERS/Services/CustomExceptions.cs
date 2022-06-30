fd47de31c4cf8fb952bd36ca31bbd339a9e283b6
Jay Sprout <jotasprout@gmail.com>
2022-06-30 04:31:39 -0400

    add exceptions

--- AutumnERS/Services/CustomExceptions.cs
+++ AutumnERS/Services/CustomExceptions.cs
@@ -1,32 +1,29 @@
 namespace customExceptions;
-
-// Look up ... PascalCase or camelCase for namepaces
-
-
-// userName already taken
-
-public class UserNameUnavailable : System.Exception
+public class ResourceNotFound : System.Exception
 {
-    public UserNameUnavailable() { }
-    public UserNameUnavailable(string message) : base(message) { }
-    public UserNameUnavailable(string message, System.Exception inner) : base(message, inner) { }
-    protected UserNameUnavailable(
+    public ResourceNotFound() { }
+    public ResourceNotFound(string message) : base(message) { }
+    public ResourceNotFound(string message, System.Exception inner) : base(message, inner) { }
+    protected ResourceNotFound(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
 
-
-// login failure
-
-public class InvalidCreds : System.Exception
+public class UsernameNotAvailable : System.Exception
 {
-    public InvalidCreds() { }
-    public InvalidCreds(string message) : base(message) { }
-    public InvalidCreds(string message, System.Exception inner) : base(message, inner) { }
-    protected InvalidCreds(
+    public UsernameNotAvailable() { }
+    public UsernameNotAvailable(string message) : base(message) { }
+    public UsernameNotAvailable(string message, System.Exception inner) : base(message, inner) { }
+    protected UsernameNotAvailable(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 }
-
-
-// no justification given for reimbursement request
+public class InvalidCredentials : System.Exception
+{
+    public InvalidCredentials() { }
+    public InvalidCredentials(string message) : base(message) { }
+    public InvalidCredentials(string message, System.Exception inner) : base(message, inner) { }
+    protected InvalidCredentials(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
